84a00d24242bfcfd2ef413a08bcd17f8fa635245
svnggggg <elperuco784@gmail.com>
2024-06-01 20:49:32 -0300

    Fix the figcaption

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let cartCount = 0;
 
-    descriptions.forEach(description => {
-        description.addEventListener('click', function() {
+    descriptions.forEach(description => { // => Función flecha - ForEach => Selecciona todos los elementos de descripción (hijo figcaption h6)
+        description.addEventListener('click', function() { // => Maneja el evento
             cartCount += 1;
             cartCounter.innerHTML = `<img src="./assets/CARRO-COMPRAS.svg" class="carrito">${cartCount}`;
 
